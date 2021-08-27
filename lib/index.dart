f1af589c35fcc5a00dd7ea62d51dd7cdb22a9bd9
Sutatta <sutatta.pakkata@gmail.com>
2021-08-27 19:26:47 +0700

    update code

--- lib/index.dart
+++ lib/index.dart
@@ -4,42 +4,39 @@ class Index extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
-    return Scaffold(
+       return Scaffold(
       body: Container(
         alignment: Alignment.center,
         child: Stack(
           children: [
             Positioned(
-              top: -140,
-              left: -140,
+              top: -100,
+              left: -120,
               child: Image.network(
-                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8osvCFF-Ni0zuzpmU9pOc6DLcoGgPfYv50tjckP-WylES9qRhOlMKUr2ESHIuvcODoNg&usqp=CAU',
-                ),
-                ),
-                 Positioned(
-              bottom: -120,
-              left: -100,
-              child: Image.asset(
-                'asset/image/gray.png',
-                ),
-                ),
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1QGxEM0qVOLy2fnd5YVckB1PlspYLpZDApw150YmF1wUIEw-9KwxBwuzlcUxHAfacJc0&usqp=CAU"
+              ),
+            ),
+            Positioned(
+              bottom: -820,
+              left: -700,
+              child: Image.network(
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Eo_circle_cyan_blank.svg/1024px-Eo_circle_cyan_blank.svg.png"
+              ),
+            ),
             Column(
               children: [
-               SizedBox(
+                SizedBox(
                   height: 20,
                 ),
                 Image.asset(
-                  "asset/image/logo-kmutnb.png",
-                  width: size.width * 0.35,
-                  height: size.height * 0.2,
-                ),
-                SizedBox(
-                  height: 20,
+                  "asset/image/logo.png",
+                  width: size.width * 0.4,
+                  height: size.height * 0.25,
                 ),
                 Text(
                   "Welcome To KMUTNB",
                   style: TextStyle(
-                    color: Colors.orange[800],
+                    color: Colors.amber[800],
                     fontSize: 30,
                     fontWeight: FontWeight.bold,
                   ),
@@ -48,25 +45,27 @@ class Index extends StatelessWidget {
                   height: 20,
                 ),
                 Image.network(
-                  'https://images.unsplash.com/photo-1629872874038-b1d600221640?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80',
-                  width: size.width * 0.55,
-                  height: size.height * 0.37,
+                  "https://images.unsplash.com/photo-1484662020986-75935d2ebc66?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80",
+                  width: size.width * 0.9,
                 ),
                 SizedBox(
-                  height: 20,
+                  height: 10,
                 ),
+
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    primary: Colors.orange[800],
+                    primary: Colors.amber[800],
                     shape: StadiumBorder(),
                     padding: EdgeInsets.fromLTRB(120, 10, 120, 10),
                   ),
                   child: Text(
-                    "LOGIN",
-                    style: TextStyle(fontSize: 16),
+                    "Login",
+                    style: TextStyle(
+                      fontSize: 20
+                    ),
                   ),
-                  onPressed: () {
-                    print("Success");
+                  onPressed: (){
+                    print("Complete");
                   },
                 ),
                 SizedBox(
@@ -74,20 +73,23 @@ class Index extends StatelessWidget {
                 ),
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    primary: Colors.orange[800],
+                    primary: Colors.amber[800],
                     shape: StadiumBorder(),
                     padding: EdgeInsets.fromLTRB(120, 10, 120, 10),
                   ),
                   child: Text(
                     "SIGN UP",
-                    style: TextStyle(fontSize: 16),
+                    style: TextStyle(
+                      fontSize: 20
+                    ),
                   ),
-                  onPressed: () {
-                    print("Thank you");
+                  onPressed: (){
+                    print("Thank You");
                   },
                 ),
+
               ],
-            ),
+            )
           ],
         ),
       ),
