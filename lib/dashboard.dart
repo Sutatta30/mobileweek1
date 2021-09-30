64fecae52767deda06a4a9d2f237975270d95b6e
Sutatta <sutatta.pakkata@gmail.com>
2021-09-30 21:39:15 +0700

    updat

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:mobileweek1/model/tct.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({Key? key}) : super(key: key);
@@ -7,11 +9,77 @@ class Dashboard extends StatefulWidget {
   _DashboardState createState() => _DashboardState();
 }
 
+var data;
+
 class _DashboardState extends State<Dashboard> {
   @override
+  void initState() {
+    super.initState();
+    print("Hello");
+    callAPI();
+  }
+
+  Future<void> callAPI() async {
+    var url = Uri.parse("https://www.boredapi.com/api/activity");
+    var response = await http.get(url);
+
+    setState(() {
+      data = tctFromJson(response.body);
+    });
+    //print(response.statusCode);
+    //print(response.body);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Dashboard'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Icon(Icons.api),
+            SizedBox(width: 10),
+            Text('Dashboard'),
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(children: [
+            Text(
+              data?.activity ?? "loading...",
+              style: TextStyle(
+                color: Colors.pink.shade300,
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0,
+              ),
+            ),
+            Text(
+              data?.type ?? "loading...",
+              style: TextStyle(
+                color: Colors.pink.shade200,
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0,
+              ),
+            ),
+            Text(
+              '${data?.price ?? "loading..."}' ?? "loading...",
+              style: TextStyle(
+                color: Colors.purple.shade300,
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0,
+              ),
+            ),
+            Text(
+              '${data?.participants ?? "loading..."}' ?? "loading...",
+              style: TextStyle(
+                color: Colors.purple.shade200,
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0,
+              ),
+            ),
+          ]),
+        ),
+      ),
     );
   }
 }
